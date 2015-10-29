875fb6e19019f691593845e4a6d28c28a8ab1fe9
Hao Wu <wuhao4u@gmail.com>
2015-10-29 17:51:23 +0800

    init

---
+++ EventArgExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+/// <summary>
+/// offer a thread safe method for raising an event
+/// <example>
+/// protected virtual viud OnNewMail(NewMailEventArgs e){
+///     e.Raise(this, ref m_NewMail);}
+/// </example>
+/// </summary>
+public static class EventArgExtensions
+{
+    public static void Raise<TEventArgs>(this TEventArgs e,
+        Object sender, ref EventHandler<TEventArgs> eventDelegate)
+    {
+        // Copy a reference to the delegate field now into a temporary field for thread safety
+        EventHandler<TEventArgs> temp = Volatile.Read(ref eventDelegate);
+
+        // If any methods registered interest with our event, notify them
+        if (temp != null) temp(sender, e);
+    }
+}
